d48eec4f9a8f71c61071bd8ee59593f3c10fad9b
Yan, Toby <Toby.Yan@kla-tencor.com>
2020-05-11 17:03:34 +0800

    Complete Demo of Singleton Design Pattern

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => GetS1Message(),
+                () => GetS2Message());
+            Console.ReadLine();
+        }
+
+        private static void GetS2Message()
+        {
+            var s2 = Singleton.Instance;
+            s2.PrintDetails("From S2 Message");
+        }
+
+        private static void GetS1Message()
+        {
+            var s1 = Singleton.Instance;
+            s1.PrintDetails("From S1 Message");
+        }
+    }
+}
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SingletonDemo
+{
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> _instance = new Lazy<Singleton>();
+        public static Singleton Instance 
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+        private Singleton() { }
+
+
+        //private static readonly Object lockObj = new object();
+
+        //         if (null == Instance)
+        //                {
+        //                    lock (lockObj)
+        //                    {
+
+        //                        _instance = new Singleton();
+        //    }
+        //}
+
+        //private static readonly Singleton _instance = new Singleton();
+
+        //public static Singleton Instance
+        //{
+        //    get => _instance;
+        //}
+
+
+        public void PrintDetails(string strMsg)
+        {
+            Console.WriteLine(strMsg);
+        }
+    }
+}
